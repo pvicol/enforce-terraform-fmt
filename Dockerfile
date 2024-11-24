34f8c7b7c0de6d31e409204d9fa302d3f564c443
pvicol <vicolp1@gmail.com>
2024-11-23 22:28:51 -0800

    Fixes for entry point

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM alpine:latest
 
-RUN apk update && apk add curl unzip git
+RUN apk update --no-cache && apk add --no-cache curl unzip git
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,35 @@
 #!/bin/sh -l
 set -e
 
+# Validate a Terraform version was passed
 if [ -z "${INPUT_TERRAFORM_VERSION}" ]; then
   echo "No terraform version specified"
   exit 1
 fi
 
-VERSION=$(expr "${INPUT_TERRAFORM_VERSION}" : [0-9]\\+\.[0-9]\\+\.[0-9]\\+)
+# Validate the Terraform version is valid
+VERSION=$(expr "${INPUT_TERRAFORM_VERSION}" : "[0-9]\\+\.[0-9]\\+\.[0-9]\\+")
 if [ "$VERSION" = "0" ]; then
   echo "Invalid terraform version specified"
   exit 1
 fi
 
-# get terraform
-curl "https://releases.hashicorp.com/terraform/${INPUT_TERRAFORM_VERSION}/terraform_${INPUT_TERRAFORM_VERSION}_linux_amd64.zip" \
---output terraform_${INPUT_TERRAFORM_VERSION}_linux_amd64.zip
+# Validate there are enough commits to compare with
+if [ "$(git rev-list --count HEAD)" -lt 2 ]; then
+  echo "Not enough commits to compare"
+  exit 0
+fi
 
+# Download Terraform
+curl -fSL "https://releases.hashicorp.com/terraform/${INPUT_TERRAFORM_VERSION}/terraform_${INPUT_TERRAFORM_VERSION}_linux_amd64.zip" \
+--output "terraform_${INPUT_TERRAFORM_VERSION}_linux_amd64.zip" || {
+  echo "Failed to download Terraform version ${INPUT_TERRAFORM_VERSION}"
+  exit 1
+}
+
+# Unzip the Terraform archive
 if [ -f "terraform_${INPUT_TERRAFORM_VERSION}_linux_amd64.zip" ]; then
-    unzip -o terraform_${INPUT_TERRAFORM_VERSION}_linux_amd64.zip
+    unzip -o "terraform_${INPUT_TERRAFORM_VERSION}_linux_amd64.zip"
 else
   echo "Couldn't find terraform version"
   exit 1
@@ -26,9 +38,9 @@ fi
 # get the changed .tf/.tf.json files
 FAILED="false"
 for FILENAME in $(git diff --name-only HEAD HEAD~1); do
-    if [ ! $(expr "${FILENAME}" : ".*\.[tf|tf.json]") = "0" ]; then
+    if [ ! "$(expr "${FILENAME}" : ".*\.[tf|tf.json]")" = "0" ]; then
         # fmt them
-        RESULT=$(./terraform fmt ${FILENAME} 2> /dev/null || echo "error")
+        RESULT=$(./terraform fmt "${FILENAME}" || echo "error")
         if [ "${RESULT}" = "${FILENAME}" ] || [ "${RESULT}" = "error" ]; then
             FAILED="true"
             echo "${FILENAME} failed"
@@ -40,7 +52,7 @@ done
 
 if [ "$FAILED" = "true" ] ; then
     # output the git diff
-    echo "::set-output name=diff::$(git diff $(git diff --name-only HEAD HEAD~1))"
+    echo "diff=$(git diff "$(git diff --name-only HEAD HEAD~1)")" >> "$GITHUB_ENV"
     exit 1
 fi
 exit 0
